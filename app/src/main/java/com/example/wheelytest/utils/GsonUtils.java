2d28a8011d7a21d68064563826f4d314a2acaafe
makavelka <vovchilnik@yandex.ru>
2016-05-10 11:14:35 +0300

    Утилиты для работы с Gson

--- app/src/main/java/com/example/wheelytest/utils/GsonUtils.java
+++ app/src/main/java/com/example/wheelytest/utils/GsonUtils.java
@@ -1,4 +1,4 @@
-package com.example.wheelytest;
+package com.example.wheelytest.utils;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
@@ -6,24 +6,24 @@ import com.google.gson.GsonBuilder;
 import java.lang.reflect.Type;
 
 public final class GsonUtils {
-    private static Gson gson;
+    private Gson gson;
 
-    public static String toJson(Object o) {
+    public String toJson(Object o) {
         gson = new GsonBuilder().create();
         return gson.toJson(o);
     }
 
-    public static <T> T fromJson(String s, Type typeOfT){
+    public <T> T fromJson(String s, Type typeOfT){
         gson = new GsonBuilder().create();
         return gson.fromJson(s, typeOfT);
     }
 
-    public static String toJsonWithNull(Object o) {
+    public String toJsonWithNull(Object o) {
         gson = new GsonBuilder().serializeNulls().create();
         return gson.toJson(o);
     }
 
-    public static <T> T fromJsonWithNull(String s, Type typeOfT){
+    public <T> T fromJsonWithNull(String s, Type typeOfT){
         gson = new GsonBuilder().serializeNulls().create();
         return gson.fromJson(s, typeOfT);
     }
