49fe964cb03e54ca1fb288a939921d3a5c062888
makavelka <vovchilnik@yandex.ru>
2016-05-12 00:45:40 +0300

    Добавлены новые значения

--- app/src/main/java/com/example/wheelytest/Values.java
+++ app/src/main/java/com/example/wheelytest/Values.java
@@ -1,6 +1,7 @@
 package com.example.wheelytest;
 
 public interface Values {
+    String BASE_URL = "ws://mini-mdt.wheely.com";
     String USERNAME_KEY = "username";
     String PASS_KEY = "password";
     double START_LAT = 55.751244;
@@ -8,4 +9,5 @@ public interface Values {
     String PREFS_TAG = "PREFS_TAG";
     String LOGIN_TAG = "LOGIN_TAG";
     String PASS_TAG = "PASS_TAG";
+    int NOTIFICATION_ID = 1337;
 }
