ff7e6c00ca8aa3a93037b274e2b5db4824a7da75
makavelka <vovchilnik@yandex.ru>
2016-05-12 00:49:41 +0300

    Базовый интерфейс для вью

--- app/src/main/java/com/example/wheelytest/view/IView.java
+++ app/src/main/java/com/example/wheelytest/view/IView.java
@@ -1,9 +1,5 @@
 package com.example.wheelytest.view;
 
-/**
- * Базовый интерфейс для слоя View, имеющий 2 метода:
- * showToast(String message) - показать информационное сообщение
- */
 public interface IView {
-    void showToast(String message);
+    void onStopEventBus();
 }
