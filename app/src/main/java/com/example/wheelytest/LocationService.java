d939e48008e02fe142c7d598bcc6c32bb9d2dc63
makavelka <vovchilnik@yandex.ru>
2016-05-12 00:47:54 +0300

    Сервис переделан под foreground service с notification, для большей живучести. Сокет вынесен в отдельный модуль. Добавлен метод, убивающий сервис вручную

--- app/src/main/java/com/example/wheelytest/LocationService.java
+++ app/src/main/java/com/example/wheelytest/LocationService.java
@@ -1,44 +1,44 @@
 package com.example.wheelytest;
 
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
-import android.location.Criteria;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
-import android.net.Uri;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
-import android.support.v4.util.ArrayMap;
-import android.widget.Toast;
+import android.support.v4.app.NotificationCompat;
 
 import com.example.wheelytest.model.GeoData;
 import com.example.wheelytest.model.LatLng;
-import com.example.wheelytest.model.db.LocationsDbDataSource;
+import com.example.wheelytest.model.StopService;
+import com.example.wheelytest.model.ws.WebSocketHelper;
 import com.example.wheelytest.utils.GsonUtils;
-import com.neovisionaries.ws.client.WebSocket;
-import com.neovisionaries.ws.client.WebSocketAdapter;
-import com.neovisionaries.ws.client.WebSocketException;
-import com.neovisionaries.ws.client.WebSocketFactory;
-import com.neovisionaries.ws.client.WebSocketFrame;
+import com.example.wheelytest.view.activity.MapActivity;
 
-import java.io.IOException;
+import org.greenrobot.eventbus.EventBus;
+import org.greenrobot.eventbus.Subscribe;
+
 import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
+import java.util.StringTokenizer;
 
 import javax.inject.Inject;
 
 public class LocationService extends Service {
 
-    @Inject GsonUtils mGsonUtils;
-
-    private LocationManager mLocationManager;
-    private LocationsDbDataSource mDataSource;
-    private WebSocket mWebSocket;
-    private boolean isConnected = false;
+    private final String mServiceName = ".LocationService";
+    @Inject
+    GsonUtils mGsonUtils;
+    @Inject
+    EventBus mEventBus;
+    @Inject
+    WebSocketHelper mWebSocketHelper;
+    @Inject
+    LocationManager mLocationManager;
 
     @Nullable
     @Override
@@ -46,23 +46,39 @@ public class LocationService extends Service {
         return null;
     }
 
-    public static String createURL(Map<String, String> params) {
-        Uri.Builder builder = Uri.parse("ws://mini-mdt.wheely.com").buildUpon();
-        for (Map.Entry<String, String> entry : params.entrySet()) {
-            builder.appendQueryParameter(entry.getKey(), entry.getValue());
-        }
-        return builder.build().toString();
+    private void start() {
+        Bitmap icon = BitmapFactory.decodeResource(getResources(),
+                R.mipmap.ic_launcher);
+        Intent notificationIntent = new Intent(this, MapActivity.class);
+        notificationIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK
+                | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0,
+                notificationIntent, 0);
+        NotificationCompat.Builder notification = new NotificationCompat.Builder(this)
+                .setContentTitle(getString(R.string.service_is_up))
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setLargeIcon(
+                        Bitmap.createScaledBitmap(icon, 128, 128, false))
+                .setContentIntent(pendingIntent)
+                .setOngoing(true);
+        startForeground(Values.NOTIFICATION_ID, notification.build());
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         App.getComponent().inject(this);
-        initWebSocket();
-        mDataSource = new LocationsDbDataSource(this);
-        mDataSource.open();
+        mEventBus.register(this);
         mLocationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
         mLocationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 2000, 5, l);
-        return START_REDELIVER_INTENT;
+        start();
+        return START_STICKY;
+    }
+
+    @Subscribe
+    public void onMessageEvent(StopService stopService){
+        stopForeground(stopService.isStop());
+        mWebSocketHelper.disconnect();
+        mEventBus.unregister(this);
     }
 
     LocationListener l = new LocationListener() {
@@ -83,56 +99,8 @@ public class LocationService extends Service {
 
         @Override
         public void onLocationChanged(Location location) {
-            String lat = String.valueOf(location.getLatitude());
-            String lon = String.valueOf(location.getLongitude());
             LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
-            mDataSource.createGeoData(lat, lon, false);
-            if (isConnected) {
-                mWebSocket.sendText(mGsonUtils.toJson(latLng));
-            }
-        }
-    };
-
-    public void initWebSocket() {
-        try {
-            Map<String, String> params = new ArrayMap<>();
-            params.put(Values.USERNAME_KEY, "a");
-            params.put(Values.PASS_KEY, "a");
-            mWebSocket = new WebSocketFactory().createSocket(createURL(params));
-            mWebSocket.addListener(mWebSocketAdapter);
-            mWebSocket.connectAsynchronously();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-    }
-
-    private WebSocketAdapter mWebSocketAdapter = new WebSocketAdapter() {
-        @Override
-        public void onConnected(WebSocket websocket, Map<String, List<String>> headers) throws Exception {
-            isConnected = true;
-            Criteria criteria = new Criteria();
-            String bestProvider = mLocationManager.getBestProvider(criteria, false);
-            android.location.Location location = mLocationManager.getLastKnownLocation(bestProvider);
-            if (location != null) {
-                mWebSocket.sendText(mGsonUtils.toJson(new LatLng(location.getLatitude(), location.getLongitude())));
-            } else {
-                mWebSocket.sendText(mGsonUtils.toJson(new LatLng(Values.START_LAT, Values.START_LON)));
-            }
-        }
-
-        @Override
-        public void onDisconnected(WebSocket websocket, WebSocketFrame serverCloseFrame, WebSocketFrame clientCloseFrame, boolean closedByServer) throws Exception {
-        }
-
-        @Override
-        public void onError(WebSocket websocket, WebSocketException cause) throws Exception {
-        }
-
-        @Override
-        public void onTextMessage(WebSocket websocket, String text) throws Exception {
-            ArrayList<GeoData> arrayList = new ArrayList<>();
-            arrayList.addAll(Arrays.asList(mGsonUtils.fromJson(text, GeoData[].class)));
-            Toast.makeText(LocationService.this, text, Toast.LENGTH_SHORT).show();
+            mWebSocketHelper.sendMessage(latLng);
         }
     };
 }
